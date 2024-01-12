63454536c46e3fef7f7914a01303292a6d292acb
CodeScratcher <arialexanderfisher@outlook.com>
2024-01-12 12:56:22 -0500

    Made roll good

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,8 +5,8 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY = -200.0
 @export var ACCEL = 100.0
 @export var DECEL = 150.0
-@export var JUMP_VARIETY = 2
-
+@export var JUMP_VARIETY = 4
+@export var ROLL_SPEED = 400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -16,6 +16,8 @@ var started_jump = false
 var finished_jump = false
 
 func _physics_process(delta):
+	var in_roll = $AnimationPlayer.current_animation == "roll"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -40,14 +42,18 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x += direction * ACCEL
-	else:
-		velocity.x = move_toward(velocity.x, 0, DECEL)
 	
-	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
-
+	if not in_roll:
+		if direction:
+			velocity.x += direction * ACCEL
+		else:
+			velocity.x = move_toward(velocity.x, 0, DECEL)
+		
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	else:
+		velocity.x = ROLL_SPEED * sign(velocity.x)
+		
 	move_and_slide()
 	
-	if Input.is_action_pressed("roll"):
+	if Input.is_action_pressed("roll") && velocity.x != 0:
 		$AnimationPlayer.play("roll")
