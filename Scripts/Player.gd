1a8432a8a460973469d8b63c6c0f154d3b2b4b67
CodeScratcher <arialexanderfisher@outlook.com>
2024-01-24 10:09:39 -0500

    Refactor Player.gd and add stamina to jumping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -25,11 +25,7 @@ var stamina = MAX_STAMINA
 var hit_iframes = 0
 var flasks = 4
 
-func _physics_process(delta):
-	var in_roll = $AnimationPlayer.current_animation == "roll" or $AnimationPlayer.current_animation == "roll_left"
-	var in_attack = $AnimationPlayer.current_animation == "attack"
-	var neutral = (not in_roll) and (not in_attack)
-	# Add the gravity.
+func handle_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		# Add to the jump timer if not on ground
@@ -37,30 +33,56 @@ func _physics_process(delta):
 		
 		if not Input.is_action_pressed("jump") and started_jump:
 			finished_jump = true
+			
 	else:
 		jump_timer = 0.0
 		started_jump = false
 		finished_jump = false
-		
-	# Handle jump.
-	if Input.is_action_pressed("jump") and jump_timer < JUMP_VARIETY and not finished_jump and neutral:
+
+func handle_jump(delta):
+	if Input.is_action_pressed("jump") and jump_timer < JUMP_VARIETY and not finished_jump and stamina > 1.5:
 		velocity.y = JUMP_VELOCITY
+		stamina -= 1.5
 		
 		# Coyote time
 		if not started_jump:
 			started_jump = true
 			jump_timer = 0.0
 
-	# Get the input direction and handle the movement/deceleration.
+func handle_movement(delta):
 	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x += direction * ACCEL
+	else:
+		velocity.x = move_toward(velocity.x, 0, DECEL)
 	
-	if neutral:
-		if direction:
-			velocity.x += direction * ACCEL
+	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+
+func handle_roll(delta):
+	if Input.is_action_just_pressed("roll") and velocity.x != 0 and is_on_floor() and stamina >= 25:
+		if velocity.x > 0:
+			$AnimationPlayer.play("roll")
 		else:
-			velocity.x = move_toward(velocity.x, 0, DECEL)
-		
-		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+			$AnimationPlayer.play("roll_left")
+		stamina -= 25
+
+func handle_attack(delta):
+	if Input.is_action_just_pressed("light") and stamina >= 10:
+		$AnimationPlayer.play("attack")
+		stamina -= 10
+
+func _physics_process(delta):
+	var in_roll = $AnimationPlayer.current_animation == "roll" or $AnimationPlayer.current_animation == "roll_left"
+	var in_attack = $AnimationPlayer.current_animation == "attack"
+	var neutral = (not in_roll) and (not in_attack)
+
+	handle_gravity(delta)
+
+	if neutral:
+		handle_movement(delta)
+		handle_jump(delta)
+		handle_roll(delta)
+		handle_attack(delta)
 	elif in_roll:
 		velocity.x = ROLL_SPEED * sign(velocity.x)
 	elif in_attack:
@@ -68,18 +90,7 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("roll") and velocity.x != 0 and is_on_floor() and neutral and stamina >= 25:
-		if velocity.x > 0:
-			$AnimationPlayer.play("roll")
-		else:
-			$AnimationPlayer.play("roll_left")
-		stamina -= 25
-		
-	if Input.is_action_just_pressed("light") and neutral and stamina >= 10:
-		$AnimationPlayer.play("attack")
-		stamina -= 10
-	
-	if neutral:
+	if neutral and not started_jump:
 		stamina += STAMINA_REGEN * delta
 	
 	stamina = clamp(stamina, 0, 100)
