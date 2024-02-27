47f368735112e2d454739cf287a5d49aa664e494
Iemonaide <kbloniasz2026@jpkeefehs.org>
2024-02-27 13:44:59 -0500

    e

--- Scripts/TestRangedEnemy.gd
+++ Scripts/TestRangedEnemy.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+
+@export var MAX_SPEED = 200.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var hp = 30
+var target = null
+var player_in_range = false
+var atk_cooldown = 2.0
+var damage = 25
+
